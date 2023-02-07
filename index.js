7e960f2af65ecae6048c2ced6afa8d1ef81883a7
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-07 11:22:30 -0700

    Fund contract

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const balanceButton = document.getElementById("balanceButton");
+const fundButton = document.getElementById("fundButton");
 connectButton.onclick = connect;
 balanceButton.onclick = getBalance;
+fundButton.onclick = fund;
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
@@ -33,4 +35,40 @@ async function getBalance() {
   } else {
     balanceButton.innerHTML = "Please install MetaMask";
   }
+}
+
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value;
+  console.log(`Funding with ${ethAmount}...`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+  return new Promise((resolve, reject) => {
+    try {
+      provider.once(transactionResponse.hash, (transactionReceipt) => {
+        console.log(
+          `Completed with ${transactionReceipt.confirmations} confirmations. `
+        );
+        resolve();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
