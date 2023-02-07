7b671ca12bd6d842ca61989b8f106365d26efe10
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-07 10:00:13 -0700

    Connect user to MetaMask

---
+++ index.js
@@ -0,0 +1,20 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+connectButton.onclick = connect;
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      return console.log(error);
+    }
+    connectButton.innerHTML = "Connected";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  } else {
+    connectButton.innerHTML = "Please install MetaMask";
+  }
+}
